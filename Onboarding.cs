464231052f5a48d2f9505eed17a5b2a264ed15cc
RPopov <altnum16@gmail.com>
2020-09-16 14:25:45 +0300

    Create Onboarding.cs
    Conditions, Standard streams

---
+++ Onboarding.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Player
+{
+    static void Main(string[] args)
+    {
+
+        while (true)
+        {
+            string enemy1 = Console.ReadLine();
+            int dist1 = int.Parse(Console.ReadLine());
+            string enemy2 = Console.ReadLine();
+            int dist2 = int.Parse(Console.ReadLine());
+
+            
+            if (dist1 < dist2) {
+                Console.WriteLine(enemy1);
+            } else {
+                Console.WriteLine(enemy2);
+            }
+
+        }
+    }
+}
